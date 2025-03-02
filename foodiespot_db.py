fc87a6803c46dc16a60bf30af0a3f48031811a3a
anurag1402 <96884376+anurag1402@users.noreply.github.com>
2025-03-02 13:09:44 +0530

    Update foodiespot_db.py

--- foodiespot_db.py
+++ foodiespot_db.py
@@ -1,18 +1,18 @@
 # foodiespot_db.py
 
 import psycopg2
-import os
-from dotenv import load_dotenv
+import streamlit as st
 from datetime import datetime
 import random
 
+
 load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_PORT = os.getenv("DB_PORT")
+DB_HOST = st.secrets["DB_HOST"]
+DB_NAME = st.secrets["DB_NAME"]
+DB_USER = st.secrets["DB_USER"]
+DB_PASSWORD = st.secrets["DB_PASSWORD"]
+DB_PORT = st.secrets["DB_PORT"]
 
 def get_connection():
     try:
